3485de3a7091e37e03964fbca91b5746275416dc
Von Mueller <vonjonmueller@gmail.com>
2026-03-02 20:29:04 -0800

    Add Leader and default split keybinds

--- wezterm.lua
+++ wezterm.lua
@@ -13,7 +13,19 @@ config.font_size = 14.0
 config.check_for_updates = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.leader = { key = '\\', mods = 'ALT', timeout_milliseconds = 1000 }
 config.keys = {
+    {
+        key = 'v',
+        mods = 'LEADER',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 's',
+        mods = 'LEADER',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+
     -- Move between split panes: Alt + h/j/k/l
     smart.split_nav('move', 'h'),
     smart.split_nav('move', 'j'),
