5d78a0fe37e3d4dd04b4424a0351c84a4a806b5a
Von Mueller <vonjonmueller@gmail.com>
2026-09-17 04:05:23 -0700

    Fix smart-split

--- smart-splits.lua
+++ smart-splits.lua
@@ -2,11 +2,18 @@ _M = {}
 
 local wezterm = require('wezterm')
 
+-- IS_NVIM is set by smart-splits.nvim. Process name is a fallback; it is
+-- often nil, and calling `:find` on nil used to abort the keybind.
 local function is_vim(pane)
-    -- This checks for the IS_NVIM var set by the plugin
-    -- Or falls back to checking the process name
-    return pane:get_user_vars().IS_NVIM == 'true' or
-    pane:get_foreground_process_name():find('n?vim') ~= nil
+    if pane:get_user_vars().IS_NVIM == 'true' then
+        return true
+    end
+    local proc = pane:get_foreground_process_name()
+    if not proc or proc == '' then
+        return false
+    end
+    local name = proc:match('[^/\\]+$') or proc
+    return name == 'nvim' or name == 'vim'
 end
 
 local direction_keys = {
@@ -22,18 +29,15 @@ _M.split_nav = function(resize_or_move, key)
         key = key,
         mods = modifier,
         action = wezterm.action_callback(function(win, pane)
-            if is_vim(pane) then
-                -- Pass the keys through to Neovim
+            local num_panes = #win:active_tab():panes()
+            if is_vim(pane) or num_panes == 1 then
                 win:perform_action({
                     SendKey = { key = key, mods = modifier },
                 }, pane)
+            elseif resize_or_move == 'resize' then
+                win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
             else
-                -- Native Wezterm split navigation/resize
-                if resize_or_move == 'resize' then
-                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-                else
-                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-                end
+                win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
             end
         end),
     }
